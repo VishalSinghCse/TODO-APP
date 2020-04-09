2f886ea7baae323e793e9cb06faf1dc0a4211bd7
vishal <dscevishal@gmail.com>
2020-04-09 15:19:17 +0530

    changes

--- todo.js
+++ todo.js
@@ -7,9 +7,9 @@ addButton.addEventListener('click',addItem)
 var removeButton=document.getElementById('remove');
 removeButton.addEventListener('click',removeitem)
 
-function addItem(params) {
+function addItem() {
     var input =document.getElementById('input');//grabbing the input from document
-    var ItemValue=input.value;
+    var item=input.value;
     ul =document.getElementById('list');
     var textnode =document.createTextNode(item); //grab list
 
@@ -17,11 +17,49 @@ function addItem(params) {
         return false;
         //Add a p tag and assign a value of "ENTER TODO
     } else{
+       
+        /* CREATE LI */
+
         //declaring checkbox  on the go //li initialized now but declared above
         li =document.createElement('li') //first we need to create an LI,then input then label
+       
+       /* CREATE CHECKBOX */
         var checkbox =document.createElement('input')
+        //input ke koi properties hone chahiye
+        checkbox.type ='checkbox';
+       
+        //setAttribute -how we can assign id,class to checkbox or p tah or h1 EventTarget
+        checkbox.setAttribute('id','check')//takes 2 parameter key, value
+
+        /* create label */ //label dont have any properties
+        var label =document.createElement('label');
+
+        //setAttribute for any property
+        label.setAttribute('for','item') //optional
+
+        //add these elements on web page
+
+        ul.appendChild(label); //generating the element
+        li.appendChild(checkbox)//
+
+        label.appendChild(textnode);//putting inside text node
+        li.appendChild(label);
+
+        ul.insertBefore(li, ul.childNodes[0]);
+        li.className ='visual';
+
+        input.value='';//clear after input
     }
 
+}
+
+function removeItem() {
+    li =ul.children
+        for (let index = 0; index < li.length; index++) {
+            while (li[index] &&li[index].children[0].checked) {
+                
+            }
+        }
 }
 
 
