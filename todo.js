2dca268809c719fc735d1c79ed39a5462a67387b
vishal <dscevishal@gmail.com>
2020-04-09 10:44:34 +0530

    day 2

--- todo.js
+++ todo.js
@@ -1,9 +1,38 @@
-alert('connected');
+var val=5;
+
+while (val>0) {
+    console.log(val);
+    val=val-1; //val--    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //ELEMENT create TEXT NODE
-<a href
 
-var h =document.createElement('h1')
-var myValue =document.createTextNode('Hello World')
 
-<h1>HELLOW </h1>
+// var h =document.createElement('h1')
+// var myValue =document.createTextNode('Hello World')
+
+// h.appendChild(myValue)
+// document.querySelector
+
+// // how to target DOM OBJECTS
+// document.getElementById(id)
+// document.getElementsByClassName(name)
+// document.getElementsByTagName(name)
